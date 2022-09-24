787e2a23ba1f0fb8250e8aed75e2ae5bb09def20
theppiwat-k <79078495+theppiwat-k@users.noreply.github.com>
2022-09-24 19:27:12 +0700

    authentication

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) { 
+  const authHeader = req.headers['authorization'] 
+  const token = authHeader && authHeader.split(' ')[1] 
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, "process.env.TOKEN_SECRET", (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+  next();
+}
+
+function generateAccessToken(username) {
+  return jwt.sign({ data: username }, "process.env.TOKEN_SECRET", {
+    expiresIn: "1h",
+  });
+}
+
+authenticateToken.unless = require("express-unless");
+
+module.exports = {
+  authenticateToken,
+  generateAccessToken,
+};
