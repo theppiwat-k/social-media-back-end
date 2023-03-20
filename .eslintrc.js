2f02de4ce382364cf7d6946018414d73dc1397ae
thepk457 <thepk457@examplcubes.postbox.in.th>
2023-03-20 11:30:06 +0700

    Add eslintrc and config prettierr

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'node': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'no-console': 'warn',
+        'eqeqeq': 'warn',
+        'camelcase': ['error',{'properties': 'never'}],
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single',
+            { 'allowTemplateLiterals': true }
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
